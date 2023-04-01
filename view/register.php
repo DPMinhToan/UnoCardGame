459af3a58bd1b744324a7bb0fbf9d455db9e93a7
Đỗ Phú Minh Tân <Tan@ISHOPnoMacBook-Pro.local>
2023-04-01 11:04:13 +0700

    Hoan thanh trang register có bat loi va dang ki tahnh cong

--- view/register.php
+++ view/register.php
@@ -1,3 +1,43 @@
+<?php
+  if(isset($_POST["submit"])){
+      session_start();
+    
+      require_once("../database/model/user.php");
+      $user = new UnoUser;
+      $user->username = $_POST["username"];
+      $user->password = $_POST["password"];
+      $user->name = $_POST["name"];
+      if($user->username == "" || $user->password == ""){
+        echo "<script> alert( 'Username and password cannot be empty!' ); </script>";
+      }
+      else if( strlen($user->username) < 6 || strlen($user->password) < 6){
+        echo "<script> alert( 'Username and password must have more than 5 character!' ); </script>";
+      }
+      else if( strlen($user->username) > 20 || strlen($user->password) > 20){
+        echo "<script> alert( 'Username and password must have less than 20 character!' ); </script>";
+      }
+      else if($user->name == ""){
+        echo "<script> alert( 'Your name cannot be empty!' ); </script>";
+      }
+      else if(strlen($user->name) > 20){
+        echo "<script> alert( 'Your name must have less than 20 character!' ); </script>";
+      }
+      else if($user->password != $_POST["confirm-password"]){
+        echo "<script> alert( 'Password and cofirm password did not match' ); </script>";
+      }
+      else{
+        require_once("../controller/userController.php");
+        $controller = new UserController();
+        if( $controller->check_user_by_username($user))
+        {
+          echo "<script> alert( 'Username has been used, please choose a new one' ); </script>";
+        }else{
+          $controller->save_user_data($user);
+          header("Location: http://localhost:3000/view/login.php");
+        }
+      }    
+    }
+?>
 
 <!DOCTYPE html>
 <html>
