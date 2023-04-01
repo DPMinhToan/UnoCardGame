5dff88d688fc77df096ea9de7af87b5cc5a89d36
Đỗ Phú Minh Tân <Tan@ISHOPnoMacBook-Pro.local>
2023-04-01 10:59:19 +0700

    Xay dung ui trang register

---
+++ view/register.php
@@ -0,0 +1,48 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="../css/login_register.css">
+</head>
+<body><div class="container">
+
+    <form id="signup" method="post">
+
+        <div class="header">
+        
+            <h3>Register</h3>
+            
+            <p>Sign-up quick, the fun's await!</p>
+            
+        </div>
+        
+        <div class="sep"></div>
+
+        <div class="inputs">
+        
+            <input type="email" name="username" placeholder="Username" autofocus />
+        
+            <input type="password" name="password" placeholder="Password" />
+
+            <input type="password" name="confirm-password" placeholder="Confirm Password" />
+
+            <input type="email" name="name" placeholder="Your In-game Name" />
+            
+            <button id="submit" type="submit" name="submit" href="#">SIGN-UP</button>
+
+            <a id="submit" href="http://localhost:3000/view/login.php">Already have an account? Sign-in now</a>
+        
+        </div>
+
+    </form>
+
+</div>
+​</body>
+</html>
+
+  
+
+
+
+</body>
+</html>
